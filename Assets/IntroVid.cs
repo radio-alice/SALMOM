1200df01bf91295dda99473bdea289dde99549be
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-03-09 13:02:54 -0600

    add videos, finalize live controls
    Former-commit-id: 793c2d1b1e894bf827ce9533a40fa5013faf9be0

---
+++ Assets/IntroVid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class IntroVid : MonoBehaviour
+{
+    VideoPlayer video;
+    void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (!video.enabled)
+            {
+                video.enabled = true;
+                video.Play();
+            } else {
+                video.enabled = false;
+                video.Stop();
+            }
+        }
+    }
+}
--- Assets/Scripts/VideoMidi.cs
+++ Assets/Scripts/VideoMidi.cs
@@ -6,46 +6,21 @@ using MidiJack;
 
 public class VideoMidi : MonoBehaviour
 {
+    public int setNote;
 
-    VideoPlayer video;
+    public GameObject[] videos;
 
     void Start()
     {
-        video = GetComponent<VideoPlayer>();
-
         MidiMaster.noteOnDelegate += NoteOn;
-        MidiMaster.knobDelegate += Knob;
     }
 
     void NoteOn(MidiChannel channel, int note, float velocity)
     {
-        if (note == 4) video.enabled = !video.enabled;
-    }
-
-    void Knob(MidiChannel channel, int knobNumber, float knobValue){
-        if (video.enabled == true){
-            Vector3 scale = transform.localScale;
-            Vector3 pos = transform.position;
-            switch (knobNumber)
-            {
-                case 21:
-                    scale.x = knobValue * 5;
-                    break;
-                case 22:
-                    scale.y = knobValue * 5;
-                    break;
-                case 23:
-                    video.playbackSpeed = knobValue * 10;
-                    break;
-                case 41:
-                    pos.x = knobValue * 5;
-                    break;
-                case 42:
-                    pos.y = knobValue * 5;
-                    break;
-            }
-            transform.localScale = scale;
-            transform.position = pos;
+        if (note >= setNote)
+        {
+            GameObject video = videos[note - setNote];
+            video.SetActive(!video.activeInHierarchy);
         }
     }
 }
